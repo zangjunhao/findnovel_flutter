971e7727e6b2c174a539cbd6df1d8efbd0e059b8
“your_username” <67698619@qq.com>
2018-12-27 18:19:50 +0800

    练手5(布局)

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,15 @@ class MyApp extends StatelessWidget{
            padding: EdgeInsets.all(20.0),
 //           margin: EdgeInsets.all(20.0),
            //color: Colors.orange,
+
            child:  TextField(
           autofocus: true,
           maxLines: 1,
           decoration: InputDecoration(
             labelText: "查询名",
-            hintText: "输入你要查询的名字或者学号"
+            labelStyle: TextStyle(fontSize: 19.0),
+            hintText: "输入你要查询的名字或者学号",
+            hintStyle: TextStyle(fontSize: 17.0)
           ),
         ),),
          Row(
@@ -49,7 +52,7 @@ class MyApp extends StatelessWidget{
                 color: Colors.orange,
                transform: Matrix4.rotationZ(-.2),
                child:
-                   DecoratedBox(decoration:BoxDecoration(gradient: LinearGradient(colors:[Colors.green,Colors.blue]),
+                   DecoratedBox(decoration:BoxDecoration(gradient: LinearGradient(colors:[Colors.lightBlueAccent,Colors.purpleAccent]),
                       // borderRadius:BorderRadius.circular(50.0)
                    ),
               child:
@@ -58,8 +61,19 @@ class MyApp extends StatelessWidget{
              color: Colors.transparent,
              splashColor: Colors.transparent,
           // shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10.0)),
-             onPressed: ()=>{},child: Text("查   询",style: TextStyle(color: Colors.black),),),))
-         ],)
+             onPressed: ()=>{},child: Text("查   询",style: TextStyle(color: Colors.black,fontSize: 16.5),),),
+                   ))
+         ],),
+        ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: <Widget>[
+            const Text('I\'m dedicating every day to you'),
+            const Text('Domestic life was never quite my style'),
+            const Text('When you smile, you knock me out, I fall apart'),
+            const Text('And I thought I was so smart'),
+          ],
+        ),
        ],
           )
       )
